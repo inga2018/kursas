f93373dd76ec490f3afa0ed2e081b1a8627bc41e
Inga <kriukovaitei@gmail.com>
2018-11-27 17:37:53 +0200

    Pridėtas namų darbas PHP01

---
+++ Namu darbas/PHP01/index01.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+<?php $šalis = 'Lietuva';
+	$miestas = 'Vilnius';
+	$populiacija = 574221;
+?>
+<ul>
+	<li><?php echo 'šalis ' . $šalis; ?></li>
+	<li><?php echo 'miestas ' . $miestas; ?></li>
+	<li><?php echo 'populiacija ' . $populiacija; ?></li>
+</ul>
+
+</body>
+</html>
