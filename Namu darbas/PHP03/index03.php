9ec4b5640eddba0a063f9572da825eb0bd48b362
Inga <kriukovaitei@gmail.com>
2018-11-27 17:41:49 +0200

    Pridėtas namų darbas PHP03

---
+++ Namu darbas/PHP03/index03.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$x = 10;
+$y = 7;
+$total = true;
+
+echo "$x + $y =" . $total=$x+$y . "<br>";
+echo "$x - $y =" . $total=$x-$y . "<br>";
+echo "$x * $y =" . $total=$x*$y . "<br>";
+echo "$x / $y =" . $total=$x/$y . "<br>";
+echo "$x % $y =" . $total=$x%$y . "<br>";
+?>
+</body>
+</html>
