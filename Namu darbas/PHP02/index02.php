4c6825f28b87bcbf6690ecefa3be5b770e3b9b93
Inga <kriukovaitei@gmail.com>
2018-11-27 17:39:49 +0200

    Pridėtas namų darbas PHP02

---
+++ Namu darbas/PHP02/index02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php $šalis = 'Lietuva';
+	$miestas = 'Vilnius';
+	$populiacija = 574221;
+	$largest_population = true; 
+
+echo $miestas . '-Lietuvos sostinė' . "<br>"; 
+echo "$miestas ir $šalis sparčiai auga"; 
+?>
+
+</body>
+</html>
