e51fece28c5a7769389b5bb440661f444cfc1086
Inga <kriukovaitei@gmail.com>
2018-11-28 22:07:15 +0200

    Pridėtas darbas PHP10

---
+++ Namu darbas/PHP10/index10.php
@@ -0,0 +1,9 @@
+<?php
+function staciakampioPlotas ($a, $b){ //Sukurkite funkciją su dviem parametrais a ir b, kurie yra stačiakampio kraštinių ilgiai.
+	$sandauga = $a * $b; //Funkcija apskaičiuos stačiakampio plotą. 
+	echo "Stačiakampio, kurio kraštinės yra $a ir $b, plotas: $sandauga"; //Rezultatą išveskite į naršyklę.
+}
+
+staciakampioPlotas (10,5);
+
+?>
